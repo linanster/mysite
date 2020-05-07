60975a11107f65d5d3be851e4cc22ba95e694fb0
llinan <believelinan@aliyun.com>
2020-05-07 10:52:05 +0800

    introduce blue_index.py, redirect / and /index to /filezilla/index

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,3 +1,5 @@
 def init_views(app):
     from app.views.blue_filezilla import blue_filezilla
+    from app.views.blue_index import blue_index
     app.register_blueprint(blue_filezilla)
+    app.register_blueprint(blue_index)
