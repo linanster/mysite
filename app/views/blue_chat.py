731dbd831b700d076183242f3a577375b7e40fbe
LiNan <believelinan@aliyun.com>
2020-05-28 18:52:59 +0800

    pass async_mode from view to html

--- app/views/blue_chat.py
+++ app/views/blue_chat.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request, render_template, flash, redirect, url_for, send_from_directory
 import os
+from app.ext.mysocketio import socketio
 
 blue_chat = Blueprint('blue_chat', __name__, url_prefix='/chat')
 
 @blue_chat.route('/')
 @blue_chat.route('/index')
 def vf_index():
-    return render_template('chat_index.html')
+    return render_template('chat_index.html', async_mode=socketio.async_mode)
